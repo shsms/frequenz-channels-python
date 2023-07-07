b4d0b29745273370c68e1dab85f7d4e89db40df2
Sahas Subramanian <sahas.subramanian@proton.me>
2023-07-07 20:03:20 +0200

    Convert to a maturin project

---
+++ src/_frequenz_channels_rust_impl/_channels_impl.pyi
@@ -0,0 +1,11 @@
+def sum_as_string(a: int, b: int) -> str:
+    """Sum two integers and return the result as a string.
+
+    Args:
+        a: The first integer.
+        b: The second integer.
+
+    Returns:
+        The sum of the two integers as a string.
+    """
+    ...
