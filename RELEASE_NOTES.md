085fba08d08abd128c0b024d1d359c07c5b1c244
Mathias L. Baumann <mathias.baumann@frequenz.com>
2024-09-18 11:05:14 +0200

    Timer: Make `stop()` and `reset()` interrupt a waiting timer
    Signed-off-by: Mathias L. Baumann <mathias.baumann@frequenz.com>

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -3,3 +3,5 @@
 ## Bug Fixes
 
 - `FileWatcher`: Fixed `ready()` method to return False when an error occurs. Before this fix, `select()` (and other code using `ready()`) never detected the `FileWatcher` was stopped and the `select()` loop was continuously waking up to inform the receiver was ready.
+
+- `Timer.stop()` and `Timer.reset()` now immediately stop the timer if it is running. Before this fix, the timer would continue to run until the next interval.
--- src/frequenz/channels/timer.py
+++ src/frequenz/channels/timer.py
@@ -523,6 +523,8 @@ class Timer(Receiver[timedelta]):
         See the documentation of `MissedTickPolicy` for details.
         """
 
+        self._reset_event = asyncio.Event()
+
         self._loop: asyncio.AbstractEventLoop = (
             loop if loop is not None else asyncio.get_running_loop()
         )
@@ -604,8 +606,13 @@ class Timer(Receiver[timedelta]):
 
         if start_delay_ms < 0:
             raise ValueError(f"`start_delay` can't be negative, got {start_delay}")
-        self._stopped = False
+
         self._next_tick_time = self._now() + start_delay_ms + self._interval
+
+        if self.is_running:
+            self._reset_event.set()
+
+        self._stopped = False
         self._current_drift = None
 
     def stop(self) -> None:
@@ -621,6 +628,7 @@ class Timer(Receiver[timedelta]):
         self._stopped = True
         # We need to make sure it's not None, otherwise `ready()` will start it
         self._next_tick_time = self._now()
+        self._reset_event.set()
 
     # We need a noqa here because the docs have a Raises section but the documented
     # exceptions are raised indirectly.
@@ -664,7 +672,15 @@ class Timer(Receiver[timedelta]):
         # could be reset while we are sleeping, in which case we need to recalculate
         # the time to the next tick and try again.
         while time_to_next_tick > 0:
-            await asyncio.sleep(time_to_next_tick / 1_000_000)
+            await next(
+                asyncio.as_completed(
+                    [
+                        asyncio.sleep(time_to_next_tick / 1_000_000),
+                        self._reset_event.wait(),
+                    ]
+                )
+            )
+            self._reset_event.clear()
             now = self._now()
             time_to_next_tick = self._next_tick_time - now
 
